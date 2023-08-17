563dafbe391aa4ae3e3528f6140ff83f877a322f
francopieruzzi64 <francopieruzzi64@gmail.com>
2023-08-17 00:39:29 -0300

    M03 TERMINADO

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -8,18 +8,22 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve false.
    // Tu código:
-   if(numero > 0){
-    return "Es positivo";
-  }else if(numero < 0){
-    return "Es negativo";
-  }else{return false;}
+  if (num > 0){
+   return "Es positivo"
+  }
+  else if (num < 0){
+   return "Es negativo"
+  }
+  else if (num === 0){
+   return false
+  }
 }
 
 function agregarSimboloExclamacion(str) {
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
- return + "!"  
+ return str + "!"  
 }
 
 function combinarNombres(nombre, apellido) {
@@ -33,8 +37,8 @@ function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
-   return "hola" + nombre + "!" 
-}
+   return `Hola ${nombre}!`
+   }
 
 function obtenerAreaRectangulo(alto, ancho) {
    // Retornar el área de un rectángulo teniendo su altura y ancho.
@@ -68,11 +72,12 @@ function esVocal(letra) {
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
    if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
-      return "es vocal";
+      return "Es vocal"
    }
    else {
-      return "dato incorrecto";
+      return "Dato incorrecto"
    }
+}
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
@@ -85,4 +90,4 @@ module.exports = {
    areaDelTriangulo,
    deEuroAdolar,
    esVocal,
-};}
+};
